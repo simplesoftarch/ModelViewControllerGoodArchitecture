3e79d6897da55cd48e6661bc29354a333d6dd4a0
Monika Koschel <mkoschel6@gmail.com>
2016-10-29 19:16:19 +0200

    Add empty Xcode project and read

---
+++ MVCGoodArchitecture/MVCGoodArchitecture/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVCGoodArchitecture
+//
+//  Created by Monika Koschel on 29.10.2016.
+//  Copyright © 2016 Monika Koschel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
