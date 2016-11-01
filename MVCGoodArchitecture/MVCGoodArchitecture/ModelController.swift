90cee04b06bff2c64dacb9b1c4c74c31ec1c11b4
Monika Koschel <lady.of.architecture@gmail.com>
2016-11-01 16:25:03 +0100

    Added saving/reading archived model items.

--- MVCGoodArchitecture/MVCGoodArchitecture/ModelController.swift
+++ MVCGoodArchitecture/MVCGoodArchitecture/ModelController.swift
@@ -6,14 +6,26 @@
 //  Copyright © 2016 Monika Koschel. All rights reserved.
 //
 
-//import Foundation
+import Foundation
 
 class ModelController {
     
-    private(set) var toDoItems: [ToDoItem] = [ToDoItem]()
+    private static let filePath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first! + "/items.txt"
+    
+    private(set) var toDoItems: [ToDoItem] = {
+        if let items = NSKeyedUnarchiver.unarchiveObject(withFile: ModelController.filePath) as? [ToDoItem] {
+            return items
+        } else {
+            return [ToDoItem]()
+        }
+    }()
     
     func addItem(item: ToDoItem) {
         toDoItems.append(item)
+    }
+    
+    func save() {
+        NSKeyedArchiver.archiveRootObject(self.toDoItems, toFile: ModelController.filePath)
     }
     
 }
