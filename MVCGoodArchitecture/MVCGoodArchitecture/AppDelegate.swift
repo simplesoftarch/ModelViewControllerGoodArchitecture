18f05c06287f1f873e7d9875d4fdaaaa99889c4d
Monika Koschel <lady.of.architecture@gmail.com>
2016-11-01 17:13:55 +0100

    Added ModelController into AppDelegate and saving items when app is inactive.

--- MVCGoodArchitecture/MVCGoodArchitecture/AppDelegate.swift
+++ MVCGoodArchitecture/MVCGoodArchitecture/AppDelegate.swift
@@ -12,21 +12,22 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    let modelController = ModelController()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        if let navigationController = window?.rootViewController as? UINavigationController, let toDoListTableViewController = navigationController.viewControllers.first as? ToDoListTableViewController {
+            toDoListTableViewController.modelController = modelController
+        }
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        modelController.save()
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        modelController.save()
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
